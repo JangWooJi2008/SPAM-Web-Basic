dcea7ecd573ff8045d78486455d3c03c243cae14
JangWooJi2008 <165270436+JangWooJi2008@users.noreply.github.com>
2024-04-11 19:11:59 +0900

    study day 3 (js.var)

---
+++ js/var.js
@@ -0,0 +1,21 @@
+let name = "장우진";
+let age = 19;
+let height = 174;
+let gf = true;
+let bf = undefined;
+let baby = null;
+
+console.log(
+  "제 이름은",
+  name,
+  "이고",
+  age,
+  "살에 키는",
+  height,
+  "여자친구는",
+  gf,
+  "남자친구는",
+  bf,
+  "얘기는",
+  baby
+);
